48446748a71bc3f1a9cc19a8a7d18246fd44f3d4
twzkraus <twzkraus@gmail.com>
2020-11-04 15:50:13 -0800

    adds hooks to app to track number of results available and number of pages

--- challenge_1/client/components/EventsApp.jsx
+++ challenge_1/client/components/EventsApp.jsx
@@ -9,10 +9,17 @@ const EventsApp = () => {
   const [events, setEvents] = useState([]);
   const [pageCount, setPageCount] = useState(1);
   const [currentPage, setCurrentPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
+  const [perPage, setPerPage] = useState(10);
 
   const searchForEvent = (text) => {
     getEvents(text)
-      .then(result => setEvents(result.data));
+      .then(result => {
+        setEvents(result.data);
+        let numRecords = parseInt(result.headers['x-total-count']);
+        setTotalResults(numRecords);
+        setPageCount(Math.ceil(numRecords / perPage));
+      });
   }
 
   const handlePageChange = (data) => {
@@ -29,7 +36,7 @@ const EventsApp = () => {
           breakClassName={'break-me'}
           pageCount={pageCount}
           marginPagesDisplayed={1}
-          pageRangeDisplayed={5}
+          pageRangeDisplayed={perPage}
           onPageChange={handlePageChange}
           containerClassName={'pagination'}
           subContainerClassName={'pages pagination'}
