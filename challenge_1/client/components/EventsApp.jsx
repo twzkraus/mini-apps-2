372d1ef3af9e8dfdc1d8f31fc3c6a1d9f97a7895
twzkraus <twzkraus@gmail.com>
2020-11-04 15:34:45 -0800

    adds react-paginate and basic event handler

--- challenge_1/client/components/EventsApp.jsx
+++ challenge_1/client/components/EventsApp.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import ReactPaginate from 'react-paginate';
 import Search from './Search.jsx';
 import List from './List.jsx';
 import { getEvents } from '../../server/json-requests.js';
@@ -6,16 +7,34 @@ import { getEvents } from '../../server/json-requests.js';
 const EventsApp = () => {
 
   const [events, setEvents] = useState([]);
+  const [pageCount, setPageCount] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
 
   const searchForEvent = (text) => {
     getEvents(text)
       .then(result => setEvents(result.data));
   }
 
+  const handlePageChange = (data) => {
+  }
+
   return (
     <>
       <Search searchEvents={searchForEvent}/>
       <List events={events}/>
+      <ReactPaginate
+          previousLabel={'previous'}
+          nextLabel={'next'}
+          breakLabel={'...'}
+          breakClassName={'break-me'}
+          pageCount={pageCount}
+          marginPagesDisplayed={1}
+          pageRangeDisplayed={5}
+          onPageChange={handlePageChange}
+          containerClassName={'pagination'}
+          subContainerClassName={'pages pagination'}
+          activeClassName={'active'}
+        />
     </>
   )
 }
