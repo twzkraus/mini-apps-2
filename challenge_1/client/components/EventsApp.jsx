7b8a04874f1b66e6f6b12b12452ef674f0ed5226
twzkraus <twzkraus@gmail.com>
2020-11-04 12:24:35 -0800

    handles search submission from Search component

--- challenge_1/client/components/EventsApp.jsx
+++ challenge_1/client/components/EventsApp.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import Search from './Search.jsx';
+import { getEvents } from '../../server/json-requests.js';
 
-const EventsApp = () => (
-  <div>Hello World</div>
-)
+const EventsApp = () => {
 
-export default <EventsApp/>;
+  const searchForEvent = (event, text) => {
+    event.preventDefault();
+    getEvents({description: text})
+      .then(result => console.log(result));
+  }
+
+  return (
+    <Search searchEvents={searchForEvent}/>
+  )
+}
+
+export default EventsApp;
