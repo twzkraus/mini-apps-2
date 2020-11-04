180e42ceb2f98043ae1fb1867c8ac5e884da7851
twzkraus <twzkraus@gmail.com>
2020-11-04 12:05:32 -0800

    Renders Hello World on app

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,2 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import EventsApp from './components/EventsApp.jsx';
 
+ReactDOM.render(EventsApp, document.getElementById("app"));
 
---
+++ challenge_1/client/components/EventsApp.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const EventsApp = () => (
+  <div>Hello World</div>
+)
+
+export default <EventsApp/>;
