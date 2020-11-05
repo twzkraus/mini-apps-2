dca3d853dd961ce23de6b3f434900f559bb6ef3e
twzkraus <twzkraus@gmail.com>
2020-11-04 19:55:18 -0800

    allows user to edit event description with a click

--- challenge_1/client/components/Event.jsx
+++ challenge_1/client/components/Event.jsx
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 
-const Event = ({ data }) => (
-  <tr>
-    <td>
-      {data.date}
-    </td>
-    <td>
-      {data.description}
-    </td>
-    <td>
-      {data.category1 && data.category2 ? data.category1 + ', ' + data.category2 : ' '}
-    </td>
-  </tr>
-)
+const Event = ({ data }) => {
+
+  const [editing, setEditing] = useState(false);
+  const [currentText, setCurrentText] = useState(data.description);
+
+  const handleStartEdit = () => {
+    setEditing(true);
+  };
+
+  const handleTextEdit = (e) => {
+    setCurrentText(e.target.value);
+  };
+
+  const handleClickSave = (e) => {
+    setEditing(false);
+  };
+
+  return (
+    <tr>
+      <td>
+        {data.date}
+      </td>
+      <td>
+        {editing ?
+        <>
+          <textarea onChange={handleTextEdit} rows="4" cols="40" value={currentText}></textarea>
+          <button onClick={handleClickSave}>Update</button>
+        </>
+        : <p onClick={handleStartEdit}>{currentText}</p> }
+
+      </td>
+      <td>
+        {data.category1 && data.category2 ? data.category1 + ', ' + data.category2 : ' '}
+      </td>
+    </tr>
+  );
+}
 
 export default Event;
