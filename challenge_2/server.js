4b473c78aa86d7578e4d011a975105dc7adff7c3
twzkraus <twzkraus@gmail.com>
2020-11-05 11:41:47 -0800

    App renders hello world. Updates server to serve static html.

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const app = express();
+const path = require('path');
 const { fetchCoindesk } = require('./fetchCoindesk.js');
+
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.resolve(__dirname, 'public')));
 
 const PORT = process.env.PORT || 3000;
 
