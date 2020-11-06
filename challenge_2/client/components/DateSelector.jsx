7931e1247b8e9f4ffa2f9368154340d354d97a44
twzkraus <twzkraus@gmail.com>
2020-11-05 20:22:45 -0800

    DateSelector computes today's date and a month ago's date and sets them as the default values

--- challenge_2/client/components/DateSelector.jsx
+++ challenge_2/client/components/DateSelector.jsx
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
 
 const DateSelector = ({ handleSubmit }) => {
-  const [dates, setDates] = useState({start: '', end: ''});
+  let today = new Date();
+  let aMonthAgo = new Date(new Date().setDate(new Date().getDate() - 30));
+  today = today.toISOString().split('T')[0];
+  aMonthAgo = aMonthAgo.toISOString().split('T')[0];
+  const [dates, setDates] = useState({start: today, end: aMonthAgo});
 
   return (
     <>
     <p>Select a date range:</p>
-      <input type="date" id="start-date"/>
-      <input type="date" id="end-date"/>
+      <input type="date" id="start-date" value={aMonthAgo}/>
+      <input type="date" id="end-date" value={today}/>
       <button onClick={(e) => handleSubmit(e, dates)}>Submit</button>
     </>
   );
