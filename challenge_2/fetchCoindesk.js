78e110098357c8672193c166e18205ff0d16f9d9
twzkraus <twzkraus@gmail.com>
2020-11-05 16:59:34 -0800

    moves fetch cleanup to fetchCoindesk module

--- challenge_2/fetchCoindesk.js
+++ challenge_2/fetchCoindesk.js
@@ -3,10 +3,12 @@ const currentURL = 'https://api.coindesk.com/v1/bpi/currentprice.json';
 
 module.exports = {
   fetchCurrent: () => {
-    return fetch(currentURL);
+    return fetch(currentURL)
+      .then(result => result.body.readableBuffer.head.data.toString());
   },
   fetchHistorical: (start, end) => {
     let histURL = `https://api.coindesk.com/v1/bpi/historical/close.json?start=${start}&end=${end}`;
-    return fetch(histURL);
+    return fetch(histURL)
+      .then(result => result.body.readableBuffer.head.data.toString());
   }
 };
