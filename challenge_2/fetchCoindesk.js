cecc8156dc3c0e7f2cdbe1ef12369d9d163f770d
twzkraus <twzkraus@gmail.com>
2020-11-05 16:55:07 -0800

    changes fetchCoindesk function name to fetchCurrent

--- challenge_2/fetchCoindesk.js
+++ challenge_2/fetchCoindesk.js
@@ -2,7 +2,11 @@ const fetch = require('node-fetch');
 const currentURL = 'https://api.coindesk.com/v1/bpi/currentprice.json';
 
 module.exports = {
-  fetchCoindesk: () => {
+  fetchCurrent: () => {
     return fetch(currentURL);
   },
+  fetchHistorical: (start, end) => {
+    let histURL = `https://api.coindesk.com/v1/bpi/historical/close.json?start=${start}&end=${end}`;
+    return fetch(histURL);
+  }
 };
