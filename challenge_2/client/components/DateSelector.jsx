8f9cbc8877fca4a725aeb5109b4f85add865d12d
twzkraus <twzkraus@gmail.com>
2020-11-05 20:06:33 -0800

    removes comments, adds text prompt of 'Select a date range'

--- challenge_2/client/components/DateSelector.jsx
+++ challenge_2/client/components/DateSelector.jsx
@@ -3,15 +3,9 @@ import React, { useState } from 'react';
 const DateSelector = ({ handleSubmit }) => {
   const [dates, setDates] = useState({start: '', end: ''});
 
-  // const handleChange = (dateType) => {
-  //   debugger;
-  //   let tempDates = dates;
-  //   tempDates[dateType] = document.getElementById(dateType + '-date');
-  //   setDates(tempDates);
-  // }
-
   return (
     <>
+    <p>Select a date range:</p>
       <input type="date" id="start-date"/>
       <input type="date" id="end-date"/>
       <button onClick={(e) => handleSubmit(e, dates)}>Submit</button>
