62d31ffe9633c237f6c1efcd21f9da1ee0394f11
twzkraus <twzkraus@gmail.com>
2020-11-05 17:01:06 -0800

    moves cleanupFetch function out of fetch functions

--- challenge_2/fetchCoindesk.js
+++ challenge_2/fetchCoindesk.js
@@ -1,14 +1,16 @@
 const fetch = require('node-fetch');
 const currentURL = 'https://api.coindesk.com/v1/bpi/currentprice.json';
 
+const cleanupFetch = (result) => result.body.readableBuffer.head.data.toString();
+
 module.exports = {
   fetchCurrent: () => {
     return fetch(currentURL)
-      .then(result => result.body.readableBuffer.head.data.toString());
+      .then(result => cleanupFetch(result));
   },
   fetchHistorical: (start, end) => {
     let histURL = `https://api.coindesk.com/v1/bpi/historical/close.json?start=${start}&end=${end}`;
     return fetch(histURL)
-      .then(result => result.body.readableBuffer.head.data.toString());
+      .then(result => cleanupFetch(result));
   }
 };
