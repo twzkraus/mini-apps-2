0844d67a5b163e96e9b8eb5a3a3fa75293f0b5b9
twzkraus <twzkraus@gmail.com>
2020-11-06 11:33:12 -0800

    renders PinSelector on app, adds 'addScore' functionality

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PinSelector from './components/PinSelector.jsx';
 
 const App = () => {
+  const addScore = (value) => {
+    console.log(value);
+  }
 
-};
+  return (
+    <PinSelector addScore={addScore}/>
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById('app'));
